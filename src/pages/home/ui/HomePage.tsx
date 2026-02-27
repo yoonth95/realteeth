3bf3ef77bfa5872bbc6f5c5183ab4de098ac1349
윤태현 <78673090+yoonth95@users.noreply.github.com>
2026-02-27 23:19:48 +0900

    Style: 시맨틱 태그 적용

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -4,9 +4,9 @@ import { BookmarkSection } from '@/widgets/bookmark-cards'
 export function HomePage() {
   return (
     <div className="flex flex-col gap-6 lg:flex-row">
-      <div className="flex min-w-0 flex-1 flex-col gap-6">
+      <section className="flex min-w-0 flex-1 flex-col gap-6">
         <WeatherSection />
-      </div>
+      </section>
       <BookmarkSection />
     </div>
   )

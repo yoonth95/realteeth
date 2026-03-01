87ced38e3b22fdb2ffd859da2aff1c223ad004db
윤태현 <78673090+yoonth95@users.noreply.github.com>
2026-03-01 22:45:17 +0900

    Chore: port 변경 5173 -> 3000

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    open: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
